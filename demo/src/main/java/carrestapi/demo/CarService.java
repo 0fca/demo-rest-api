d67f4476555ea00d6918f5f3c825c409e6d4a476
Lukas <lukasbownik99@gmail.com>
2023-11-22 14:57:39 +0100

    Update CarService.java

--- demo/src/main/java/carrestapi/demo/CarService.java
+++ demo/src/main/java/carrestapi/demo/CarService.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+// TODO: Ten serwis jest dokładną repliką repozytorium, które jest pustym interface'em. 
+// To bez sensu, w tak płaskim przypadku można spłaszczyć kod zupełnie
 @Service
 public class CarService {
 
